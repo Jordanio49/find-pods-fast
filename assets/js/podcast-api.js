9b7abc1fa6f815266e69a3f4a5db842fd6126053
Jordan Thomas <jthomas135@hotmail.com>
2022-04-30 11:06:40 -0700

    starting work on podcast API and create a feature branch for it

--- assets/js/podcast-api.js
+++ assets/js/podcast-api.js
@@ -1,1 +1,21 @@
-Jordan
+var searchBtn = document.getElementById('search button element');
+var searchText = document.getElementById('search textarea');
+
+
+var searchPods = function() {
+    searchBtn.addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        const podcastApi = require('podcast-api');
+        
+        const apiUrl = podcastApi({API key here});
+        apiUrl.fetchBestPodcast({
+            name: 'searchText.textcontent'
+            genre-id: ''
+            page: 
+            region: 'us'
+            
+
+        })
+    })
+}
