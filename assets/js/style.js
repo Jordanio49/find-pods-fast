cb71116591c4b27418e54962cd0986391b77d44d
Macy Kay Mannix <macymannix@gmail.com>
2022-04-30 23:07:46 -0700

    ls loop

--- assets/js/style.js
+++ assets/js/style.js
@@ -3,10 +3,15 @@ var searchInputEl = document.getElementById('autocomplete-input');
 var searchBtnEl = document.getElementById('search-btn');
 var searchHistoryEl = document.getElementById('recent-search');
 var favesEl = document.getElementById('faves');
+var stateInputEl = document.getElementById('state-input');
 
 searchBtnEl.addEventListener('click', function(event) {
     event.preventDefault();
+    var key = searchInputEl.value;
+    var value = stateInputEl.value;
     console.log(searchInputEl.value)
+    console.log(stateInputEl.value)
+    localStorage.setItem(key, value);
    
     var listEl = document.createElement('li');
     listEl.innerHTML = "<div >"
@@ -14,3 +19,15 @@ searchBtnEl.addEventListener('click', function(event) {
     searchHistoryEl.appendChild(listEl);
     // console.log(searchInputEl.value)
 });
+
+for (let i = 0; i < localStorage.length; i++) {
+    var key = localStorage.key(i);
+    var value = localStorage.getItem(key);
+    listEl.innerHTML += '${key} <br />';
+    listEl.textContent = searchInputEl.value;
+    searchHistoryEl.appendChild(listEl);
+}
+
+
+// key = searchInputEl
+// value = 
