e2c71f5670850e5e58188fb6a52ca3e17c65e897
Jordan Thomas <jthomas135@hotmail.com>
2022-05-02 13:32:51 -0700

    adjusting HTML to popuylate cards

--- assets/js/podcast-api.js
+++ assets/js/podcast-api.js
@@ -1,8 +1,17 @@
 var searchBtn = document.getElementById('search-btn');
 var searchText = document.getElementById('autocomplete-input')
-var podcastImage = document.getElementById('image')
+var podcastImage1 = document.getElementById('image-1')
+var podcastLink1 = document.getElementById('pod-link-1')
+var podcastTitle1 = document.getElementById('title-1')
+var podcastImage2 = document.getElementById('image-2')
+var podcastLink2 = document.getElementById('pod-link-2')
+var podcastTitle2 = document.getElementById('title-2')
+var podcastImage3 = document.getElementById('image-3')
+var podcastLink3 = document.getElementById('pod-link-3')
+var podcastTitle3 = document.getElementById('title-3')
 
 searchBtn.addEventListener('click', (e) => {
+    e.preventDefault()
     getDataFromItunes()
 })
 
@@ -19,7 +28,15 @@ function getDataFromItunes() {
         console.log(json.results[0].collectionName)
         console.log(json.results[0].feedUrl)
 
-        var picture = json.results[0].artworkUrl100
-        podcastImage.innerHtml = "<img src='" + picture + "'>";
+        podcastImage1.setAttribute('src', json.results[0].artworkUrl600);
+        podcastLink1.setAttribute('href', json.results[0].collectionViewUrl);
+        podcastTitle1.textContent = json.results[0].collectionName;
+        podcastImage2.setAttribute('src', json.results[1].artworkUrl600);
+        podcastLink2.setAttribute('href', json.results[1].collectionViewUrl);
+        podcastTitle2.textContent = json.results[1].collectionName;
+        podcastImage3.setAttribute('src', json.results[2].artworkUrl600);
+        podcastLink3.setAttribute('href', json.results[2].collectionViewUrl);
+        podcastTitle3.textContent = json.results[2].collectionName;
+    
     })
 }
