c0885a2cf3a0cd1e62d619de49f5ec3fde57ca55
Jordan Thomas <jthomas135@hotmail.com>
2022-05-02 12:55:26 -0700

    figuring out the itunes API and how to get the data I need

--- assets/js/podcast-api.js
+++ assets/js/podcast-api.js
@@ -1,21 +1,25 @@
-var searchBtn = document.getElementById('search button element');
-var searchText = document.getElementById('search textarea');
+var searchBtn = document.getElementById('search-btn');
+var searchText = document.getElementById('autocomplete-input')
+var podcastImage = document.getElementById('image')
+
+searchBtn.addEventListener('click', (e) => {
+    getDataFromItunes()
+})
 
 
-var searchPods = function() {
-    searchBtn.addEventListener('click', function(event) {
-        event.preventDefault();
-        
-        const podcastApi = require('podcast-api');
-        
-        const apiUrl = podcastApi({API key here});
-        apiUrl.fetchBestPodcast({
-            name: 'searchText.textcontent'
-            genre-id: ''
-            page: 
-            region: 'us'
-            
+function getDataFromItunes() {
+    var apiUrl = 'https://itunes.apple.com/search?term=' + searchText.value + '&entity=podcast'
+    fetch(apiUrl)
+    .then(data => data.json())
+    .then(json => {
+        console.log(json)
+        console.log(json.results[0].genres)
+        console.log(json.results[0].collectionViewUrl)
+        console.log(json.results[0].artworkUrl100)
+        console.log(json.results[0].collectionName)
+        console.log(json.results[0].feedUrl)
 
-        })
+        var picture = json.results[0].artworkUrl100
+        podcastImage.innerHtml = "<img src='" + picture + "'>";
     })
 }
