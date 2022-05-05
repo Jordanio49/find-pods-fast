0f5eb70fcfe88c9f8478f7bb20a3c6fdb5d54bf4
Jordan Thomas <jthomas135@hotmail.com>
2022-05-04 18:53:34 -0700

    working on ticket master data populating our cards

--- assets/js/podcast-api.js
+++ assets/js/podcast-api.js
@@ -12,42 +12,42 @@ function getDataFromItunes() {
     fetch(apiUrl)
         .then(data => data.json())
         .then(json => {
-            // console.log(json)
-            // console.log(json.results[0].genres)
-            // console.log(json.results[0].collectionViewUrl)
-            // console.log(json.results[0].artworkUrl100)
-            // console.log(json.results[0].collectionName)
-            // console.log(json.results[0].feedUrl)
-
             for (let i = 0; i < json.results.length; i++) {
-                var podcastNames = json.results[i].collectionName;
+                var podcastTitles = json.results[i].collectionName;
                 var podcastLinks = json.results[i].collectionViewUrl;
                 var podcastImages = json.results[i].artworkUrl600;
-                // console.log(podcastNames)
+                var title = document.getElementById('title-' + [i])
+                var link = document.getElementById('pod-link-' + [i])
+                var image = document.getElementById('image-' + [i])
+                var upcomingShows = document.getElementById('upcoming-shows-' + [i])
 
-                var podcastTitle = document.getElementById('title-' + [i])
-                var podcastLink = document.getElementById('pod-link-' + [i])
-                var podcastImage = document.getElementById('image-' + [i])
-                podcastTitle.textContent = podcastNames
-                podcastLink.setAttribute('href', podcastLinks)
-                podcastImage.setAttribute('src', podcastImages)
+                title.textContent = podcastTitles
+                link.setAttribute('href', podcastLinks)
+                image.setAttribute('src', podcastImages)
+
+                $.ajax({
+                    type: "GET",
+                    url: "https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=hVqnL1G7fRfOFT0cqthaFdUpZ5C4mZJ8&size=100&keyword=" + podcastTitles,
+                    async: true,
+                    dataType: "json",
+                    success: function (json) {
+                        console.log(json);
+                        // for (let i = 0; i < json._embedded.attractions.length; i++) {
+                            var livePodcasts = json._embedded.attractions[0].name
+                            var ticketUrl = json._embedded.attractions[0].url
+                            console.log(livePodcasts)
+                            console.log(ticketUrl)
+                            if (livePodcasts) {
+                                upcomingShows.innerHTML = "There are Upcoming Shows!"
+                                console.log(ticketUrl)
+                            }
+                            else{
+                                upcomingShows.innerHTML = "There are no shows Upcoming"
+                            }
+                        // }
+                    }
+                })
             }
 
-            $.ajax({
-                type: "GET",
-                url: "https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=hVqnL1G7fRfOFT0cqthaFdUpZ5C4mZJ8&size=100&classificationName=podcast",
-                async: true,
-                dataType: "json",
-                success: function (json) {
-                    console.log(json);
-                    for (let i = 0; i < json._embedded.attractions.length; i++) {
-                        var livePodcasts = json._embedded.attractions[i].name
-                        // console.log(livePodcasts)
-                        if (livePodcasts === podcastNames) {
-                            console.log(livePodcast)
-                        }
-                    }
-                }
-            })
         })
-}
+    }
