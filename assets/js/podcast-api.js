902c10391e45185acb31dccf7c38e3e9a054b13e
Macy Kay Mannix <macymannix@gmail.com>
2022-05-07 14:06:10 -0700

    ls for both apis

--- assets/js/podcast-api.js
+++ assets/js/podcast-api.js
@@ -1,116 +1,238 @@
-var searchBtn = document.getElementById('search-btn');
-var searchText = document.getElementById('autocomplete-input')
+var searchBtn = document.getElementById("search-btn");
+var searchText = document.getElementById("autocomplete-input");
 var searchHistoryEl = document.getElementById('recent-search')
 
+var podcastTitles = [];
+var showTitles = [];
 
-searchBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    getDataFromItunes(event);
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  getApiData();
 });
 
-// function calls itunes API for pod cards, saves seach to local storage, and creates button dynamically
-function getDataFromItunes(event) {
+function getApiData() {
+  var appleApiUrl =
+    "https://itunes.apple.com/search?term=" +
+    searchText.value +
+    "&entity=podcast&limit=8";
+  fetch(appleApiUrl)
+    .then((data) => data.json())
+    .then((json) => {
+      for (let i = 0; i < json.results.length; i++) {
+        podcastTitles.push(json.results[i].collectionName);
+        // podcastTitles = json.results[i].collectionName;
+        var podcastLinks = json.results[i].collectionViewUrl;
+        var podcastImages = json.results[i].artworkUrl600;
+        var title = document.getElementById("title-" + [i]);
+        var link = document.getElementById("pod-link-" + [i]);
+        var image = document.getElementById("image-" + [i]);
+        title.textContent = podcastTitles[i];
+        link.setAttribute("href", podcastLinks);
+        image.setAttribute("src", podcastImages);
+      }
+      console.log(podcastTitles);
+      var ticketmasterApiUrl =
+        "https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=hVqnL1G7fRfOFT0cqthaFdUpZ5C4mZJ8&size=100&classificationName=podcast";
+      fetch(ticketmasterApiUrl)
+        .then((data) => data.json())
+        .then((json) => {
+          // console.log(json)
+          for (let i = 0; i < json._embedded.attractions.length; i++) {
+            showTitles.push(json._embedded.attractions[i].name);
+          }
+          console.log(showTitles);
+          checkLiveEvents();
+          podcastTitles = [];
+          showTitles = [];
+        });
+    });
 
-                var apiUrl = 'https://itunes.apple.com/search?term=' + searchText.value + '&entity=podcast&limit=8'
-                    fetch(apiUrl)
-                 .then(data => data.json())
-                 .then(json => {
-            for (let i = 0; i < json.results.length; i++) {
-                var podcastTitles = json.results[i].collectionName;
-                var podcastLinks = json.results[i].collectionViewUrl;
-                var podcastImages = json.results[i].artworkUrl600;
-                var title = document.getElementById('title-' + [i])
-                var link = document.getElementById('pod-link-' + [i])
-                var image = document.getElementById('image-' + [i])
-                title.textContent = podcastTitles
-                link.setAttribute('href', podcastLinks)
-                image.setAttribute('src', podcastImages)                    
-            }
+  var genre;
+  if (searchText.value != "") {
+    genre = searchText.value;
+    console.log(storeGenre.includes(genre));
 
-            //heres the buttons!
-            var genre;
-            if (searchText.value != "" ) {
-              genre = searchText.value;
-                console.log(storeGenre.includes(genre))
-
-              if (storeGenre.includes(genre) !== true) {
-                storeGenre.push(genre);
-                localStorage.setItem('genre', JSON.stringify(storeGenre));
-                var btn = document.createElement('button');
-                btn.className += ' waves-effect waves-light btn purple accent-1';
-                btn.id += 'toggle'
-                btn.innerText = genre;
-                btn.value = genre;
-                searchHistoryEl.appendChild(btn);
-                var br = document.createElement('br');
-                searchHistoryEl.appendChild(br);
-              }
-            }
-        })
+    if (storeGenre.includes(genre) !== true) {
+      storeGenre.push(genre);
+      localStorage.setItem("genre", JSON.stringify(storeGenre));
+      var btn = document.createElement("button");
+      btn.className += " waves-effect waves-light btn purple accent-1";
+      btn.id += "toggle";
+      btn.innerText = genre;
+      btn.value = genre;
+      searchHistoryEl.appendChild(btn);
+      var br = document.createElement("br");
+      searchHistoryEl.appendChild(br);
     }
-    
-// getting data from local 
-let storeGenre = JSON.parse(localStorage.getItem('genre')) || [];
+  }
+}
+
+function checkLiveEvents() {
+  for (i = 0; i < showTitles.length; i++) {
+    for (j = 0; j < podcastTitles.length; j++) {
+      var upcomingShows = document.getElementById("upcoming-shows-" + [j]);
+      if (showTitles[i] === podcastTitles[j]) {
+        upcomingShows.textContent =
+          "There are upcoming live events! Check ticketmaster for more details.";
+        console.log("Found shows");
+        return;
+      }
+      upcomingShows.textContent =
+        "Unfortunately there are no scheduled live events";
+    }
+  }
+}
+
+// getting data from local
+let storeGenre = JSON.parse(localStorage.getItem("genre")) || [];
 
 // forloop global and runs when page loads
 for (let i = 0; i < storeGenre.length; i++) {
-    var btn = document.createElement("button");
-    btn.className += ' waves-effect waves-light btn purple accent-1';
-    btn.id += 'toggle'
-    btn.innerText = storeGenre[i];
-    btn.value = storeGenre[i];
-    searchHistoryEl.appendChild(btn);
-    var br = document.createElement("br");
-    searchHistoryEl.appendChild(br);
+  var btn = document.createElement("button");
+  btn.className += " waves-effect waves-light btn purple accent-1";
+  btn.id += "toggle";
+  btn.innerText = storeGenre[i];
+  btn.value = storeGenre[i];
+  searchHistoryEl.appendChild(btn);
+  var br = document.createElement("br");
+  searchHistoryEl.appendChild(br);
 }
 
+// var searchBtn = document.getElementById('search-btn');
+// var searchText = document.getElementById('autocomplete-input')
+// var searchHistoryEl = document.getElementById('recent-search')
 
-// trying to get toggle to change cards :(
- var toggle = document.getElementById('toggle')
+// var podcastTitles = []
+// var showTitles = []
 
-toggle.addEventListener('click', (e) => {
-    e.preventDefault();
-    buttonGet(e);
-});
+// searchBtn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     getApiData();
+// });
 
-function buttonGet(e) {
-    var apiUrl = 'https://itunes.apple.com/search?term=' + btn.innerText + '&entity=podcast&limit=8'
-        fetch(apiUrl)
-     .then(data => data.json())
-     .then(json => {
-for (let i = 0; i < json.results.length; i++) {
-    var podcastTitles = json.results[i].collectionName;
-    var podcastLinks = json.results[i].collectionViewUrl;
-    var podcastImages = json.results[i].artworkUrl600;
-    var title = document.getElementById('title-' + [i])
-    var link = document.getElementById('pod-link-' + [i])
-    var image = document.getElementById('image-' + [i])
-    title.textContent = podcastTitles
-    link.setAttribute('href', podcastLinks)
-    image.setAttribute('src', podcastImages)
-        }
-    })
-}
+// // function calls itunes API for pod cards, saves seach to local storage, and creates button dynamically
+// function getApiData() {
 
+//  var appleApiUrl = 'https://itunes.apple.com/search?term=' + searchText.value + '&entity=podcast&limit=8'
+//                     fetch(appleApiUrl)
+//                  .then(data => data.json())
+//          .then(json => {
+//             for (let i = 0; i < json.results.length; i++) {
+//                 podcastTitles.push(json.results[i].collectionName);
+//                 // var podcastTitles = json.results[i].collectionName;
+//                 var podcastLinks = json.results[i].collectionViewUrl;
+//                 var podcastImages = json.results[i].artworkUrl600;
+//                 var title = document.getElementById('title-' + [i])
+//                 var link = document.getElementById('pod-link-' + [i])
+//                 var image = document.getElementById('image-' + [i])
+//                 title.textContent = podcastTitles[i]
+//                 link.setAttribute('href', podcastLinks)
+//                 image.setAttribute('src', podcastImages)
+//             }
 
+//             // console.log(podcastTitles)
+//             var ticketmasterApiUrl = "https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=hVqnL1G7fRfOFT0cqthaFdUpZ5C4mZJ8&size=100&classificationName=podcast"
+//             fetch(ticketmasterApiUrl)
+//                 .then(data => data.json())
+//                 .then(json => {
+//                     // console.log(json)
+//                     for (let i = 0; i < json._embedded.attractions.length; i++) {
+//                         showTitles.push(json._embedded.attractions[i].name)
+//                     }
+//                     // console.log(showTitles)
+//                     checkLiveEvents();
+//                     podcastTitles = []
+//                     showTitles = []
+//                 })
+//                 var genre;
+//                 if (searchText.value != "" ) {
+//                   genre = searchText.value;
+//                     console.log(storeGenre.includes(genre))
 
+//                   if (storeGenre.includes(genre) !== true) {
+//                     storeGenre.push(genre);
+//                     localStorage.setItem('genre', JSON.stringify(storeGenre));
+//                     var btn = document.createElement('button');
+//                     btn.className += ' waves-effect waves-light btn purple accent-1';
+//                     btn.id += 'toggle'
+//                     btn.innerText = genre;
+//                     btn.value = genre;
+//                     searchHistoryEl.appendChild(btn);
+//                     var br = document.createElement('br');
+//                     searchHistoryEl.appendChild(br);
+//                   }}
 
-        //this is for the tickmaster API i commented this out because i wanted all the cards to populate
-                // $.ajax({
-                //     type: "GET",
-                //     url: "https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=hVqnL1G7fRfOFT0cqthaFdUpZ5C4mZJ8&size=100&keyword=" + podcastTitles,
-                //     async: true,
-                //     dataType: "json",
-                //     // success: function (json) {
-                //     //     console.log(json);
-                //     //         var livePodcasts = json._embedded.attractions[0].name
-                //     //         console.log(livePodcasts)
-                            
-                //     //         // if (livePodcasts) {
-                //     //         //     upcomingShows.innerHTML = "There are Upcoming Shows! Check Ticket Master for more info."
-                //     //         // }
-                //     //         // else {
-                //     //         //     upcomingShows.innerHTML = "There are no shows Upcoming"
-                //     //         // }
-                //     // }
-                // })
+//             //heres the buttons!
+
+//             })
+//         ];
+
+// // trying to get toggle to change cards
+//  var toggle = document.getElementById('toggle')
+
+// // toggle.addEventListener('click', (e) => {
+// //     e.preventDefault();
+// //     buttonGet(e);
+// // });
+
+// // function buttonGet(e) {
+// //     var appleApiUrl = 'https://itunes.apple.com/search?term=' + btn.innerText + '&entity=podcast&limit=8'
+// //         fetch(appleApiUrl)
+// //      .then(data => data.json())
+// //      .then(json => {
+// //     for (let i = 0; i < json.results.length; i++) {
+// //         podcastTitles = push(json.results[i].collectionName);
+// //         var podcastLinks = json.results[i].collectionViewUrl;
+// //         var podcastImages = json.results[i].artworkUrl600;
+// //       var title = document.getElementById('title-' + [i])
+// //      var link = document.getElementById('pod-link-' + [i])
+// //      var image = document.getElementById('image-' + [i])
+// //      title.textContent = podcastTitles[i]
+// //      link.setAttribute('href', podcastLinks)
+// //      image.setAttribute('src', podcastImages)
+// //         }
+// //         console.log(podcastTitles)
+// //         var ticketmasterApiUrl = "https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=hVqnL1G7fRfOFT0cqthaFdUpZ5C4mZJ8&size=100&classificationName=podcast"
+// //         fetch(ticketmasterApiUrl)
+// //             .then(data => data.json())
+// //             .then(json => {
+// //                 // console.log(json)
+// //                 for (let i = 0; i < json._embedded.attractions.length; i++) {
+// //                     showTitles.push(json._embedded.attractions[i].name)
+// //                 }
+// //                 console.log(showTitles)
+// //                 checkLiveEvents();
+// //                 podcastTitles = []
+// //                 showTitles = []
+// //             })
+// //     })
+// // };
+
+// function checkLiveEvents() {
+//     for (i = 0; i < showTitles.length; i++) {
+//         for (j = 0; j < podcastTitles.length; j++) {
+//             var upcomingShows = document.getElementById('upcoming-shows-' + [j])
+//             if (showTitles[i] === podcastTitles[j]) {
+//                 upcomingShows.textContent = ('There are upcoming live events! Check ticketmaster for more details.')
+//                 console.log('Found shows')
+//                 return;
+//             }
+//             upcomingShows.textContent = 'Unfortunately there are no scheduled live events'
+//         }
+//     }
+// };
+
+// // getting data from local
+// let storeGenre = JSON.parse(localStorage.getItem('genre')) || [];
+
+// // forloop global and runs when page loads
+// for (let i = 0; i < storeGenre.length; i++) {
+//     var btn = document.createElement("button");
+//     btn.className += ' waves-effect waves-light btn purple accent-1';
+//     btn.id += 'toggle'
+//     btn.innerText = storeGenre[i];
+//     btn.value = storeGenre[i];
+//     searchHistoryEl.appendChild(btn);
+//     var br = document.createElement("br");
+//     searchHistoryEl.appendChild(br);
+// }
