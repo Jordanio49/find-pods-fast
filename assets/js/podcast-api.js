e56bac211e63b09f8ab11f86463c57919980c3e8
Macy Kay Mannix <macymannix@gmail.com>
2022-05-07 02:52:11 -0700

    stuck on toggle buttons

--- assets/js/podcast-api.js
+++ assets/js/podcast-api.js
@@ -1,6 +1,7 @@
 var searchBtn = document.getElementById('search-btn');
 var searchText = document.getElementById('autocomplete-input')
 var searchHistoryEl = document.getElementById('recent-search')
+var toggle = document.getElementById('toggle')
 
 searchBtn.addEventListener('click', (event) => {
     event.preventDefault();
@@ -21,7 +22,6 @@ function getDataFromItunes(event) {
                 var title = document.getElementById('title-' + [i])
                 var link = document.getElementById('pod-link-' + [i])
                 var image = document.getElementById('image-' + [i])
-                // var upcomingShows = document.getElementById('upcoming-shows-' + [i])
                 title.textContent = podcastTitles
                 link.setAttribute('href', podcastLinks)
                 image.setAttribute('src', podcastImages)
@@ -35,17 +35,16 @@ function getDataFromItunes(event) {
                   if (storeGenre.includes(genre) == true) {
                     var btn = document.createElement('button');
                     btn.className += ' waves-effect waves-light btn purple accent-1';
+                    btn.id += 'toggle'
                     btn.innerText = genre;
                     btn.value = genre;
-                    btn.addEventListener('click', getDataFromItunes);
                     searchHistoryEl.appendChild(btn);
                     var br = document.createElement('br');
                     searchHistoryEl.appendChild(br);
                   }
                 } else { 
                 }
-
-                //this is for the tickmaster API i commented this out because i wanted all the cards to populate
+        //this is for the tickmaster API i commented this out because i wanted all the cards to populate
                 // $.ajax({
                 //     type: "GET",
                 //     url: "https://app.ticketmaster.com/discovery/v2/attractions.json?apikey=hVqnL1G7fRfOFT0cqthaFdUpZ5C4mZJ8&size=100&keyword=" + podcastTitles,
@@ -76,10 +75,41 @@ let storeGenre = JSON.parse(localStorage.getItem('genre')) || [];
 for (let i = 0; i < storeGenre.length; i++) {
     var btn = document.createElement("button");
     btn.className += ' waves-effect waves-light btn purple accent-1';
+    btn.id += 'toggle'
     btn.innerText = storeGenre[i];
     btn.value = storeGenre[i];
-    btn.addEventListener('click', getDataFromItunes)
     searchHistoryEl.appendChild(btn);
     var br = document.createElement("br");
     searchHistoryEl.appendChild(br);
 }
+
+
+
+
+
+
+// trying to get toggle to change cards :(
+
+// toggle.addEventListener('click', (e) => {
+//     getDataFromItunes(e);
+// });
+
+// function buttonGet() {
+//     var apiUrl = 'https://itunes.apple.com/search?term=' + searchText.value + '&entity=podcast&limit=8'
+//         fetch(apiUrl)
+//      .then(data => data.json())
+//      .then(json => {
+// for (let i = 0; i < json.results.length; i++) {
+//     var podcastTitles = json.results[i].collectionName;
+//     var podcastLinks = json.results[i].collectionViewUrl;
+//     var podcastImages = json.results[i].artworkUrl600;
+//     var title = document.getElementById('title-' + [i])
+//     var link = document.getElementById('pod-link-' + [i])
+//     var image = document.getElementById('image-' + [i])
+//     // var upcomingShows = document.getElementById('upcoming-shows-' + [i])
+//     title.textContent = podcastTitles
+//     link.setAttribute('href', podcastLinks)
+//     image.setAttribute('src', podcastImages)
+//         }
+//     })
+// }
